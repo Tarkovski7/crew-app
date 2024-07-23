1449099c664453367eede88761b4fc1971c334c8
Tarkovski7 <trkbgr1618@gmail.com>
2024-07-23 17:53:56 +0300

    "Performed update operation. View card adjusted. Fixed errors in the list"

--- src/app/crew-card/crew-card.component.ts
+++ src/app/crew-card/crew-card.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Crew } from '../models/crew';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CrewService } from '../crew.service';
 
 @Component({
   selector: 'app-crew-card',
   templateUrl: './crew-card.component.html',
-  styleUrl: './crew-card.component.scss'
+  styleUrl: './crew-card.component.scss',
 })
 export class CrewCardComponent implements OnInit {
-  crew!: Crew;
-  constructor(private route: ActivatedRoute , private crewService: CrewService) {
-
-  }
+  crew?: Crew;
+  constructor(
+    private route: ActivatedRoute,
+    private crewService: CrewService,
+    private router: Router
+  ) {}
   ngOnInit(): void {
-      this.route.paramMap.subscribe(params => {
-        const id = Number(params.get('id'));
-        this.crewService.getCrewById(id).subscribe(crew  => this.crew = crew);
-      })
+    this.route.params.subscribe((params) => {
+      const id = +params['id'];
+      this.crewService.getCrewById(id).subscribe((crew) => {
+        this.crew = crew;
+      });
+    });
+  }
+  goBack() {
+    this.router.navigate(['/crew-list']);
   }
 }
--- src/app/crew.service.ts
+++ src/app/crew.service.ts
@@ -1,23 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Crew, Currency } from './models/crew';
 import { BehaviorSubject, Observable, of } from 'rxjs';
+import { Certificate } from './models/certificate';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CrewService {
   private crewData: Crew[] = [
-    new Crew(
-      1,
-      'John',
-      'Doe',
-      'American',
-      'Captain',
-      30,
-      100,
-      Currency.USD,
-      []
-    ),
+    new Crew(1, 'John', 'Doe', 'American', 'Captain', 30, 100, Currency.USD, [
+      new Certificate('First Aid', new Date('2020-01-01')),
+      new Certificate('Safety Training', new Date('2021-02-01')),
+      new Certificate('Marine Engineering', new Date('2019-03-01')),
+      new Certificate('Navigation', new Date('2018-04-01')),
+      new Certificate('Communication', new Date('2022-05-01')),
+    ]),
     new Crew(
       2,
       'Jeyn',
@@ -27,7 +24,13 @@ export class CrewService {
       25,
       90,
       Currency.EUR,
-      []
+      [
+        new Certificate('First Aid', new Date('2020-01-01')),
+        new Certificate('Safety Training', new Date('2021-02-01')),
+        new Certificate('Marine Engineering', new Date('2019-03-01')),
+        new Certificate('Navigation', new Date('2018-04-01')),
+        new Certificate('Communication', new Date('2022-05-01')),
+      ]
     ),
     new Crew(
       3,
@@ -38,7 +41,13 @@ export class CrewService {
       20,
       85,
       Currency.USD,
-      []
+      [
+        new Certificate('First Aid', new Date('2020-01-01')),
+        new Certificate('Safety Training', new Date('2021-02-01')),
+        new Certificate('Marine Engineering', new Date('2019-03-01')),
+        new Certificate('Navigation', new Date('2018-04-01')),
+        new Certificate('Communication', new Date('2022-05-01')),
+      ]
     ),
     new Crew(
       4,
@@ -49,7 +58,13 @@ export class CrewService {
       15,
       95,
       Currency.EUR,
-      []
+      [
+        new Certificate('First Aid', new Date('2020-01-01')),
+        new Certificate('Safety Training', new Date('2021-02-01')),
+        new Certificate('Marine Engineering', new Date('2019-03-01')),
+        new Certificate('Navigation', new Date('2018-04-01')),
+        new Certificate('Communication', new Date('2022-05-01')),
+      ]
     ),
     new Crew(
       5,
@@ -60,22 +75,37 @@ export class CrewService {
       10,
       80,
       Currency.USD,
-      []
+      [
+        new Certificate('First Aid', new Date('2020-01-01')),
+        new Certificate('Safety Training', new Date('2021-02-01')),
+        new Certificate('Marine Engineering', new Date('2019-03-01')),
+        new Certificate('Navigation', new Date('2018-04-01')),
+        new Certificate('Communication', new Date('2022-05-01')),
+      ]
     ),
   ];
   private crewSubject = new BehaviorSubject<Crew[]>(this.crewData);
   crew$ = this.crewSubject.asObservable();
   constructor() {}
+  getCrew(): Observable<Crew[]> {
+    return of(this.crewData);
+  }
   getCrewById(id: number): Observable<Crew> {
     const crew = this.crewData.find((c) => c.id === id);
     return of(crew!);
   }
   updateCrew(updatedCrew: Crew): Observable<void> {
-    const index = this.crewData.findIndex(c => c.id === updatedCrew.id);
+    const index = this.crewData.findIndex((c) => c.id === updatedCrew.id);
     if (index !== -1) {
-      this.crewData[index] = updatedCrew;
+      // Kopya oluşturup güncellenmiş crew verisini koyuyoruz
+      this.crewData = [
+        ...this.crewData.slice(0, index),
+        updatedCrew,
+        ...this.crewData.slice(index + 1),
+      ];
       this.crewSubject.next(this.crewData);
     }
-    return of(undefined); // Gerçek uygulamada bir API çağrısı olabilir
+    return of(undefined);
   }
+
 }
