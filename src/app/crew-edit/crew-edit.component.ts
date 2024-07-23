f120bc0c5a62a7e6b733eb87457124fc05da845c
Tarkovski7 <trkbgr1618@gmail.com>
2024-07-23 16:19:20 +0300

    fixed errors in the crew list

--- src/app/crew-edit/crew-edit.component.ts
+++ src/app/crew-edit/crew-edit.component.ts
@@ -1,17 +1,51 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Crew, Currency } from '../models/crew';
+import { CrewService } from '../crew.service';
 
 @Component({
   selector: 'app-crew-edit',
   templateUrl: './crew-edit.component.html',
-  styleUrl: './crew-edit.component.scss'
+  styleUrls: ['./crew-edit.component.scss'],
 })
 export class CrewEditComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data : any) {
-
+  crewForm: FormGroup;
+  currencies = Object.values(Currency);
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { crew: Crew },
+    private fb: FormBuilder,
+    private crewService: CrewService,
+    public dialogRef: MatDialogRef<CrewEditComponent>
+  ) {
+    this.crewForm = this.fb.group({
+      firstName: [data.crew.firstName, Validators.required],
+      lastName: [data.crew.lastName, Validators.required],
+      nationality: [data.crew.nationality, Validators.required],
+      title: [data.crew.title, Validators.required],
+      daysOnBoard: [data.crew.daysOnBoard, Validators.required],
+      dailyRate: [data.crew.dailyRate, Validators.required],
+      currency: [data.crew.currency, Validators.required],
+    });
   }
+  onSave() {
+    if (this.crewForm.valid) {
+      const updatedCrew: Crew = {
+        ...this.data.crew,
+        ...this.crewForm.value,
+      };
+      this.crewService.updateCrew(updatedCrew).subscribe(() => {
+        this.dialogRef.close(updatedCrew);
+      });
+    }
+  }
+
+
+  onCancel(): void {
+    this.dialogRef.close(false);
+  }
+
   onNoClick(): void {
-    // Implement this method if necessary
-  }
 
+  }
 }
